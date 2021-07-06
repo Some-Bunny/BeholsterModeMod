1799921169fd0e6adad2c17d29570282616a5d2f
Some-Bunny <70037543+Some-Bunny@users.noreply.github.com>
2021-07-06 17:46:30 +0100

    Initial commit

---
+++ Behaviors/AddBeholsterBeamComponent.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using ItemAPI;
+using Dungeonator;
+using System.Reflection;
+using Random = System.Random;
+using FullSerializer;
+using System.Collections;
+using Gungeon;
+using MonoMod.RuntimeDetour;
+using MonoMod;
+using System.Collections.ObjectModel;
+
+using UnityEngine.Serialization;
+
+
+namespace BeholsterMode
+{
+	public class AddBeholsterBeamComponent : BraveBehaviour
+	{
+		public AddBeholsterBeamComponent()
+        {
+			//this.ShootSFX = "Play_ENM_deathray_shot_01";
+			//this.LoopSFXBeamStop = "Stop_ENM_deathray_loop_01";
+			//this.ChargeEntity = "Play_ENM_beholster_charging_01";
+			//this.ChargeBeam = "Play_ENM_deathray_charge_01";
+			//this.FiresDirectlyTowardsPlayer = false;
+			//this.CustomAngleValue = 0;
+			//this.UsesCustomAngle = false;
+		}
+		public static GameObject shootpoint;
+		public static GameObject shootpoin1;
+
+		public void Start()
+		{
+			shootpoint = new GameObject("fuck");
+			shootpoint.transform.parent = base.aiActor.transform;
+			shootpoint.transform.position = base.aiActor.sprite.WorldCenter + new Vector2(0, 0);
+			GameObject m_CachedGunAttachPoint = base.aiActor.transform.Find("fuck").gameObject;
+
+
+
+
+			AIBeamShooter2 yeet = base.aiActor.gameObject.AddComponent<AIBeamShooter2>();
+			AIActor actor = EnemyDatabase.GetOrLoadByGuid("4b992de5b4274168a8878ef9bf7ea36b");
+			BeholsterController aIBeamShooter22 = actor.GetComponent<BeholsterController>();
+
+
+			yeet.beamTransform = m_CachedGunAttachPoint.transform;
+			yeet.beamModule = aIBeamShooter22.beamModule;
+			yeet.beamProjectile = aIBeamShooter22.beamModule.GetCurrentProjectile();
+			yeet.beamProjectile = aIBeamShooter22.projectile;
+			//yeet.LaserAngle = 90;
+			yeet.SetLaserAngle(0);
+			yeet.firingEllipseCenter = m_CachedGunAttachPoint.transform.position;
+			yeet.name = "dont overrwite pls :)";
+
+			CustomBeholsterLaserBehavior yee = new CustomBeholsterLaserBehavior()
+			{
+
+				AttackCooldown = 4,
+				InitialCooldown = 3,
+				//degTurnRateAcceleration = 30f,
+				//beamSelection = ShootBeamBehavior.BeamSelection.Specify,
+				firingTime = 5f,
+				//stopWhileFiring = false,
+				//restrictBeamLengthToAim = false,
+				RequiresLineOfSight = false,
+				trackingType = CustomBeholsterLaserBehavior.TrackingType.Follow,
+				//initialAimType = ShootBeamBehavior.InitialAimType.Aim,
+				//specificBeamShooter = aIBeamShooter,
+				//FireAnimation = null,
+				//PostFireAnimation = null,
+				//TellAnimation = null
+
+				unitCatchUpSpeed = 8f,
+				maxTurnRate = 10,
+				turnRateAcceleration = 24,
+				useDegreeCatchUp = transform,
+				minDegreesForCatchUp = 15,
+				degreeCatchUpSpeed = 180,
+				useUnitCatchUp = true,
+				minUnitForCatchUp = 2,
+				maxUnitForCatchUp = 10,
+				useUnitOvershoot = true,
+				minUnitForOvershoot = 1,
+				unitOvershootTime = 0.25f,
+				unitOvershootSpeed = 10,
+
+
+				chargeTime = 1f,
+				ShootPoint = shootpoint.transform,
+				FiresDirectlyTowardsPlayer = false,
+				//specificBeamShooter = yeet,
+				//UsesCustomAngle = true,
+				//FiresDirectlyTowardsPlayer = true,
+				UsesBaseSounds = true,
+				beamSelection = ShootBeamBehavior.BeamSelection.All,
+				//UsesBaseSounds = false,
+				DefualtDirection = true,
+				//BulletScript = new CustomBulletScriptSelector(typeof(Wailer.Wail)),
+			};
+
+			base.aiActor.behaviorSpeculator.AttackBehaviors.Add(yee);
+		}
+
+		public bool AddsBaseBeamBehavior;
+
+	}
+}
---
+++ Module.cs
@@ -0,0 +1,79 @@
+﻿using ItemAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Reflection;
+using Gungeon;
+using MonoMod.RuntimeDetour;
+
+namespace BeholsterMode
+{
+    public class BeholsterModule : ETGModule
+    {
+        public static readonly string MOD_NAME = "Beholster Mode";
+        public static readonly string VERSION = "1.0";
+        public static readonly string TEXT_COLOR = "#850000";
+
+        public override void Start()
+        {
+            ETGModConsole.Commands.AddUnit("beholstermode", delegate (string[] e)
+            {
+                bool flag = BeholsterModule.BeholsterModeHook != null;
+                if (flag)
+                {
+                    BeholsterModule.BeholsterModeHook.Dispose();
+                    BeholsterModule.BeholsterModeHook = null;
+                    ETGModConsole.Log(BraveUtility.RandomElement<string>(YoureUnFucked), false);
+                }
+                else
+                {
+                    BeholsterModule.BeholsterModeHook = new Hook(typeof(AIActor).GetMethod("Awake", BindingFlags.Instance | BindingFlags.Public), typeof(BeholsterModule).GetMethod("HandleCustomEnemyChanges"));
+                    ETGModConsole.Log(BraveUtility.RandomElement<string>(YoureFucked), false);
+                }
+            });
+            Log($"{MOD_NAME} v{VERSION} started successfully.", TEXT_COLOR);
+        }
+
+        public static void HandleCustomEnemyChanges(Action<AIActor> orig, AIActor self)
+        {
+
+            orig(self);
+            try
+            {
+                self.gameObject.AddComponent<AddBeholsterBeamComponent>();
+            }
+            catch
+            {
+                ETGModConsole.Log("Something went wrong.");
+            }
+        }
+        public static List<string> YoureFucked = new List<string>
+        {
+            "Why.",
+            "I hope you know what you're doing.",
+            "This wont end well",
+            "I'll grab the popcorn",
+            "Masochist.",
+            "Please don't do this to yourself."
+        };
+
+        public static List<string> YoureUnFucked = new List<string>
+        {
+            "I can't blame you honestly.",
+            "Good choice.",
+            "And don't turn it on again.",
+            "Hope you had fun!"
+        };
+        private static Hook BeholsterModeHook;
+
+        public static void Log(string text, string color="#FFFFFF")
+        {
+            ETGModConsole.Log($"<color={color}>{text}</color>");
+        }
+
+        public override void Exit() { }
+        public override void Init() { }
+    }
+}
